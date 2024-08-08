cf2cc63a2d340a8e3dd7eac645e8023ec98aafb9
madnan08 <adnan.sarfraz.2302105@gmail.com>
2024-08-08 13:31:49 +0000

    Updated start.sh

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ echo "MySQL is ready."
 if [ ! -f /var/www/magento2/app/etc/env.php ]; then
     echo "Running Magento setup:install..."
     php /var/www/magento2/bin/magento setup:install \
-        --base-url=http://localhost \
+        --base-url=http://localhost:9000 \
         --db-host="$DB_HOST" \
         --db-name="$DB_NAME" \
         --db-user="$DB_USER" \
