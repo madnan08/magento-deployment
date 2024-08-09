05e981da541d7c949767c30a82a2d7b5602c8682
madnan08 <adnan.sarfraz.2302105@gmail.com>
2024-08-09 06:49:36 +0000

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 
 
-echo "Waiting for MySQL to be ready..."
-until mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e "show databases;" > /dev/null 2>&1; do
-    echo -n "."
-    sleep 1
-done
-echo "MySQL is ready."
-
-
-if [ ! -f /var/www/magento2/app/etc/env.php ]; then
-    echo "Running Magento setup:install..."
-    php /var/www/magento2/bin/magento setup:install \
-        --base-url=http://localhost:9000 \
-        --db-host="$DB_HOST" \
-        --db-name="$DB_NAME" \
-        --db-user="$DB_USER" \
-        --db-password="$DB_PASSWORD" \
-        --admin-firstname=Admin \
-        --admin-lastname=User \
-        --admin-email=admin@your-domain.com \
-        --admin-user=admin \
-        --admin-password=admin123 \
-        --language=en_US \
-        --currency=USD \
-        --timezone=America/Chicago \
-        --use-rewrites=1
-fi
+echo "Running Magento setup:install..."
+php /var/www/magento2/bin/magento setup:install \
+    --base-url=http://localhost:9000 \
+    --db-host="$DB_HOST" \
+    --db-name="$DB_NAME" \
+    --db-user="$DB_USER" \
+    --db-password="$DB_PASSWORD" \
+    --admin-firstname=Admin \
+    --admin-lastname=User \
+    --admin-email=admin@your-domain.com \
+    --admin-user=admin \
+    --admin-password=admin123 \
+    --language=en_US \
+    --currency=USD \
+    --timezone=America/Chicago \
+    --use-rewrites=1
 
 
 echo "Starting PHP-FPM..."
