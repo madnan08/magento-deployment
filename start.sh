6ca945dc9dfe3b8c4c5e76dd7cd15f6d345660f7
madnan08 <adnan.sarfraz.2302105@gmail.com>
2024-08-09 12:47:06 +0000

    Updated magento-deployment.yaml

--- start.sh
+++ start.sh
@@ -2,21 +2,21 @@
 
 echo "Running Magento setup:install..."
 php /var/www/magento2/bin/magento setup:install \
-    --base-url=http://20.51.207.214:9000 \
-    --db-host="$DB_HOST" \
-    --db-name="$DB_NAME" \
-    --db-user="$DB_USER" \
-    --db-password="$DB_PASSWORD" \
+    --base-url=${MAGENTO_HOST} \
+    --db-host=${DB_HOST} \
+    --db-name=${DB_NAME} \
+    --db-user=${DB_USER} \
+    --db-password=${DB_PASSWORD} \
     --admin-firstname=Admin \
     --admin-lastname=User \
     --admin-email=adnan.sarfraz.2302105@gmail.com \
-    --admin-user=admin \
-    --admin-password=$ADMIN_PASSWORD \
+    --admin-user=${MAGENTO_USERNAME} \
+    --admin-password=${MAGENTO_PASSWORD} \
     --language=en_US \
     --currency=USD \
     --timezone=America/Chicago \
     --use-rewrites=1 \
-    --elasticsearch-host=elasticsearch \
+    --elasticsearch-host=${ELASTICSEARCH_HOST} \
     --elasticsearch-port=9200
 
 php-fpm -F
